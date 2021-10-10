4fb4f4b76e7d73a784319925a2cbd683579d62c0
lotusline <aziphoto@gmail.com>
2021-10-10 16:36:27 +0200

    initial commit

---
+++ gqlapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+
+class Product(models.Model):
+    segment = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    product = models.CharField(max_length=100)
+    units = models.IntegerField()
+    sales = models.IntegerField()
+    date_sold = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.product
+
